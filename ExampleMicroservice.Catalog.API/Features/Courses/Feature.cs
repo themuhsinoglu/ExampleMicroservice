1b3328b852212dfc3ec14a3fe1a2a2be23062349
themuhsinoglu <muhsinogluahmet@gmail.com>
2024-11-14 16:49:27 +0300

    gereksiz dosya silinmesi

--- ExampleMicroservice.Catalog.API/Features/Courses/Feature.cs
+++
@@ -1,8 +0,0 @@
-namespace ExampleMicroservice.Catalog.API.Features.Courses;
-
-public class Feature 
-{
-    public string EducatorFullName { get; set; } =default!;
-    public float Rating { get; set; }
-    public int Duration { get; set; }
-}
--- ExampleMicroservice.Catalog.API/Repositories/BaseEntity.cs
+++
@@ -1,8 +0,0 @@
-using MongoDB.Bson.Serialization.Attributes;
-
-namespace ExampleMicroservice.Catalog.API.Repositories;
-
-public class BaseEntity
-{
-    [BsonElement("_id")] public Guid Id { get; set; }
-}
